a009dcbfa5faca60e218003d88a57d7b935496ba
kei184 <ichihachiyon184@gmail.com>
2025-03-26 20:15:10 +0900

    Update app_message.js

--- app_message.js
+++ app_message.js
@@ -1,15 +1,4 @@
-if (navigator.userAgent.indexOf('Instagram') > -1) {
-  // Instagramアプリ内ブラウザで開かれた場合
-  var message = document.createElement('div');
-  message.textContent = 'Instagramアプリ内で開いています。外部ブラウザで開くには、右上の「…」から「ブラウザで開く」を選択してください。';
-  message.style.position = 'fixed';
-  message.style.top = '0';
-  message.style.left = '0';
-  message.style.width = '100%';
-  message.style.backgroundColor = 'white';
-  message.style.zIndex = '9999';
-  document.body.appendChild(message);
-} else if (navigator.userAgent.indexOf('TikTok') > -1) {
+if (navigator.userAgent.indexOf('TikTok') > -1) {
   // TikTokアプリ内ブラウザで開かれた場合
   var message = document.createElement('div');
   message.textContent = 'TikTokアプリ内で開いています。外部ブラウザで開くには、右上の「…」から「ブラウザで開く」を選択してください。';
