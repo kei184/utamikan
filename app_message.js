d6463ace9a33f07f3d535618ee8488e676037a6e
kei184 <ichihachiyon184@gmail.com>
2025-03-26 20:39:02 +0900

    Update app_message.js

--- app_message.js
+++ app_message.js
@@ -1,3 +1,17 @@
+window.onload = function() {
+  if (navigator.userAgent.toLowerCase().indexOf('tiktok') > -1) {
+    // TikTokアプリ内ブラウザで開かれた場合
+    var message = document.createElement('div');
+    message.textContent = 'TikTokアプリ内で開いています。外部ブラウザで開くには、右上の「…」から「ブラウザで開く」を選択してください。';
+    message.style.position = 'fixed';
+    message.style.top = '0';
+    message.style.left = '0';
+    message.style.width = '100%';
+    message.style.backgroundColor = 'white';
+    message.style.zIndex = '99999'; // z-indexを大きくする
+    document.body.appendChild(message);
+  }
+};
 if (navigator.userAgent.indexOf('TikTok') > -1) {
   // TikTokアプリ内ブラウザで開かれた場合
   var message = document.createElement('div');
