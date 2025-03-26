fc4bcb559cc5d9f681839d5195d759d320422649
kei184 <ichihachiyon184@gmail.com>
2025-03-26 21:10:00 +0900

    Update app_message.js

--- app_message.js
+++ app_message.js
@@ -3,38 +3,38 @@ if (navigator.userAgent.indexOf('TikTok') > -1) {
     var message = document.createElement('div');
     message.textContent = 'TikTokアプリ内で開いています。外部ブラウザで開くには、右上の「…」から「ブラウザで開く」を選択してください。';
     message.style.position = 'fixed';
-    message.style.top = '10px'; // 上からのマージン
-    message.style.left = '10px'; // 左からのマージン
-    message.style.width = 'calc(100% - 20px)'; // 幅を調整
+    message.style.top = '10px';
+    message.style.left = '10px';
+    message.style.right = '10px'; // 右側にもマージンを指定
     message.style.backgroundColor = 'white';
     message.style.zIndex = '9999';
-    message.style.padding = '10px'; // パディングを追加
-    message.style.borderRadius = '5px'; // 角を丸くする
+    message.style.padding = '10px';
+    message.style.borderRadius = '5px';
     document.body.appendChild(message);
 } else if (navigator.userAgent.indexOf('Twitter') > -1 || navigator.userAgent.indexOf('XApp') > -1) {
     // Xアプリ内ブラウザで開かれた場合
     var message = document.createElement('div');
     message.textContent = 'Xアプリ内で開いています。外部ブラウザで開くには、右上の「…」から「ブラウザで開く」を選択してください。';
     message.style.position = 'fixed';
-    message.style.top = '10px'; // 上からのマージン
-    message.style.left = '10px'; // 左からのマージン
-    message.style.width = 'calc(100% - 20px)'; // 幅を調整
+    message.style.top = '10px';
+    message.style.left = '10px';
+    message.style.right = '10px'; // 右側にもマージンを指定
     message.style.backgroundColor = 'white';
     message.style.zIndex = '9999';
-    message.style.padding = '10px'; // パディングを追加
-    message.style.borderRadius = '5px'; // 角を丸くする
+    message.style.padding = '10px';
+    message.style.borderRadius = '5px';
     document.body.appendChild(message);
 } else if (navigator.userAgent.indexOf('trill') > -1) {
     // trillアプリ内ブラウザで開かれた場合
     var message = document.createElement('div');
-    message.textContent = 'TikTokアプリ内で開いています。外部ブラウザで開くには、右上の「…」から「ブラウザで開く」を選択してください。';
+    message.textContent = 'trillアプリ内で開いています。外部ブラウザで開くには、右上の「…」から「ブラウザで開く」を選択してください。';
     message.style.position = 'fixed';
-    message.style.top = '10px'; // 上からのマージン
-    message.style.left = '10px'; // 左からのマージン
-    message.style.width = 'calc(100% - 20px)'; // 幅を調整
+    message.style.top = '10px';
+    message.style.left = '10px';
+    message.style.right = '10px'; // 右側にもマージンを指定
     message.style.backgroundColor = 'white';
     message.style.zIndex = '9999';
-    message.style.padding = '10px'; // パディングを追加
-    message.style.borderRadius = '5px'; // 角を丸くする
+    message.style.padding = '10px';
+    message.style.borderRadius = '5px';
     document.body.appendChild(message);
 }
