a7667bdbea5f7bcb7287a22abaac804e016bed3c
kei184 <ichihachiyon184@gmail.com>
2025-04-12 17:28:56 +0900

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,14 +1,22 @@
-window.addEventListener('load', function() {
-    var gtagScript = document.createElement('script');
-    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-W8VREJZ0RY";
-    gtagScript.async = true;
-    gtagScript.defer = true;
-    document.head.appendChild(gtagScript);
+document.addEventListener('DOMContentLoaded', function() {
+    try {
+        var gtagScript = document.createElement('script');
+        gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-W8VREJZ0RY";
+        gtagScript.async = true;
+        gtagScript.defer = true;
+        document.head.appendChild(gtagScript);
 
-    gtagScript.onload = function() {
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-W8VREJZ0RY');
-    };
+        gtagScript.onload = function() {
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-W8VREJZ0RY');
+        };
+
+        gtagScript.onerror = function() {
+            console.error('GTM script failed to load.');
+        };
+    } catch (error) {
+        console.error('Error loading GTM script:', error);
+    }
 });
